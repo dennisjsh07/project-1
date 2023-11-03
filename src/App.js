098a3d7bd49be2c929d0b1d685cc29248c0591b1
dennis <dennisjshofficial@gmail.com>
2023-11-03 12:40:14 +0530

    added proper date fromat and loop

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
+  const expenses = [
+    { id: 1, title: "rent", amount: 17500, date: new Date(2023, 11, 3) },
+    { id: 2, title: "food", amount: 5000, date: new Date(2023, 11, 3) },
+    { id: 3, title: "gym", amount: 1000, date: new Date(2023, 11, 3) },
+    { id: 4, title: "water", amount: 500, date: new Date(2023, 11, 3) },
+  ];
   return (
     <div className="App">
-      <p>lets start building applications</p>
-      <ExpenseItem />
+      {expenses.map((i) => (
+        <ExpenseItem
+          key={i.id} // Make sure to add a unique key when mapping in React
+          title={i.title}
+          amount={i.amount}
+          date={i.date}
+        />
+      ))}
     </div>
   );
 }
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,16 +1,21 @@
 import './ExpenseItem.css';
 
-const ExpenseItem = () => {
-    const expenseDate = new Date(2023, 11, 3);
-    const expenseTitle = 'rent';
-    const expenseAmount = 17500;
+const ExpenseItem = (data) => {
+    const year = data.date.getFullYear();
+    const month = data.date.toLocaleString('en-US', {month: 'long'})
+    const day = data.date.toLocaleString('en-US', {day: '2-digit'})
+
     return (
         <div className="expense-item">
-        <div>{expenseDate.toISOString()}</div>
-        <div className="expense-item__description">
-            <h2>{expenseTitle}</h2>
+        <div>
+            <div>{year}</div>
+            <div>{month}</div>
+            <div>{day}</div>
         </div>
-        <div className="expense-item__price">{expenseAmount}</div>
+        <div className="expense-item__description">
+            <h2>{data.title}</h2>
+        </div>
+        <div className="expense-item__price">{data.amount}</div>
         </div>
     );
 };
