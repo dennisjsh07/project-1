f7bb9bf98f476c29ab8233d6ab02419af91a7613
dennis <dennisjshofficial@gmail.com>
2023-11-04 07:55:58 +0530

    added date componenet

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,23 +1,16 @@
-import './ExpenseItem.css';
+import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate";
 
-const ExpenseItem = (data) => {
-    const year = data.date.getFullYear();
-    const month = data.date.toLocaleString('en-US', {month: 'long'})
-    const day = data.date.toLocaleString('en-US', {day: '2-digit'})
-
-    return (
-        <div className="expense-item">
-        <div>
-            <div>{year}</div>
-            <div>{month}</div>
-            <div>{day}</div>
-        </div>
-        <div className="expense-item__description">
-            <h2>{data.title}</h2>
-        </div>
-        <div className="expense-item__price">{data.amount}</div>
-        </div>
-    );
+const ExpenseItem = (props) => {
+  return (
+    <div className="expense-item">
+        <ExpenseDate date={props.date}/>
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+      </div>
+      <div className="expense-item__price">{props.amount}</div>
+    </div>
+  );
 };
 
 export default ExpenseItem;
