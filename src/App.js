aae728f15e2a8e9eb1bace63690004881f3afa7d
dennis <dennisjshofficial@gmail.com>
2023-11-04 12:20:01 +0530

    added expense form

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 
 import Expenses from "./components/Expenses/Expenses";
+import newExpense from "./components/NewExpense/NewExpense"
+import NewExpense from "./components/NewExpense/NewExpense";
 
 function App() {
   const expenses = [
@@ -11,6 +13,7 @@ function App() {
   ];
   return (
     <div>
+      <NewExpense />
       <Expenses items={expenses}/>
     </div>
   );
